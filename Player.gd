00b3105ce8d784c04f95c13d9fe5bd82d8ff1508
Toby Clarke <teeohbee@gmail.com>
2024-03-12 21:38:17 +0000

    Update player movement - Add collision detection - Animate movement between tiles

---
+++ Player.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+# Constants
+const TILE_SIZE = 16
+const ANIMATION_SPEED = 4
+const TWEEN_DURATION = 1.0
+
+# Variables
+var inputs = {
+	"ui_right": Vector2.RIGHT, "ui_left": Vector2.LEFT, "ui_up": Vector2.UP, "ui_down": Vector2.DOWN
+}
+var moving = false
+
+@onready var ray = $RayCast2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = position.snapped(Vector2.ONE * TILE_SIZE)
+	position += Vector2.ONE * TILE_SIZE / 2
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	handle_movement()
+
+# Handles player movement
+func handle_movement():
+	if moving:
+		return
+	for dir in inputs.keys():
+		if Input.is_action_pressed(dir) and not is_colliding(dir):
+			var tween = create_tween()
+			play_tween(tween, dir)
+			moving = true
+			await tween.finished
+			moving = false
+
+# Checks if the player will collide in the given direction
+func is_colliding(dir):
+	ray.target_position = inputs[dir] * TILE_SIZE
+	ray.force_raycast_update()
+	return ray.is_colliding()
+
+# Plays the tween for the player movement
+func play_tween(tween, dir):
+	(
+		tween
+		. tween_property(
+			self, "position", position + inputs[dir] * TILE_SIZE, TWEEN_DURATION / ANIMATION_SPEED
+		)
+		. set_trans(Tween.TRANS_SINE)
+	)
