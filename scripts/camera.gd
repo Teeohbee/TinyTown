44624900b34e57da9a71ea182cf1ec5f0ad576ed
Toby Clarke <teeohbee@gmail.com>
2024-04-05 13:58:38 +0100

    Remove unnecessary functions

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,16 +1,6 @@
 extends Camera2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass  # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-
 func update_player_hud():
 	update_label("Health", "HP: %d/%d" % [PlayerState.health, PlayerState.max_health])
 	update_label(
